acba1c457eeab155ad7dbdfb533030005553cda4
Matheus1415 <115950084+Matheus1415@users.noreply.github.com>
2024-03-20 19:50:55 -0300

    Atualizando evento

--- src/components/Calendario/index.tsx
+++ src/components/Calendario/index.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 import { IEvento } from '../../interfaces/IEvento';
 import style from './Calendario.module.scss';
 import ptBR from './localizacao/ptBR.json'
-import Kalend, { CalendarView } from 'kalend'
+import Kalend, { CalendarEvent, CalendarView, OnEventDragFinish } from 'kalend'
 import 'kalend/dist/styles/index.css';
 import { ListDeEventosState } from '../../state/atom';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 interface IKalendEvento {
   id?: number
@@ -35,6 +35,28 @@ const Calendario= () => {
       color: 'blue'
     })
   })
+
+  const setListaDeEventos = useSetRecoilState(ListDeEventosState);
+
+  const onEventoDragFinish: OnEventDragFinish = (
+    kalendEventoInalterado: CalendarEvent,
+    kalendEventoAtualisado: CalendarEvent
+  ) =>{
+    const evento = eventos.find(evento => evento.descricao === kalendEventoAtualisado.summary);
+    if(evento){
+      const eventoAtualizado = {...evento};
+      eventoAtualizado.inicio = new Date(kalendEventoAtualisado.startAt);
+      eventoAtualizado.inicio = new Date(kalendEventoAtualisado.endAt);
+
+      setListaDeEventos(listaAtiga => {
+        const indice = listaAtiga.findIndex(evt => evt.id === evento.id);
+        return[...listaAtiga.slice(0, indice), eventoAtualizado, ...listaAtiga.slice(indice + 1)]
+      });
+    }
+  };
+
+
+
   return (
     <div className={style.Container}>
       <Kalend
@@ -47,6 +69,7 @@ const Calendario= () => {
         calendarIDsHidden={['work']}
         language={'customLanguage'}
         customLanguage={ptBR}
+        onEventDragFinish={onEventoDragFinish}
       />
     </div>
   );
