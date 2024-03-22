5bd4aa09279592025b2c31a53710a23c09b61b08
Matheus1415 <115950084+Matheus1415@users.noreply.github.com>
2024-03-22 19:40:31 -0300

    pequenos erros

--- src/state/hooks/useAdicionarEvento.tsx
+++ src/state/hooks/useAdicionarEvento.tsx
@@ -9,7 +9,7 @@ const useAdicionarEvento = () => {
         const hoje = new Date();
         if (evento.inicio < hoje){
             throw new Error("Evento não pode ser cadastrado com data menor que a atual");
-        }
+        }   
         evento.id = obterId();
         return setListaDeEventos(listaAntiga => [...listaAntiga, evento]);
     }
