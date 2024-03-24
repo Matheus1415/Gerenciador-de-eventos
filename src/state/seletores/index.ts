3830ec74f131134e2bc2db308d8923ab6b690ddf
Matheus1415 <115950084+Matheus1415@users.noreply.github.com>
2024-03-24 11:07:07 -0300

    função async

--- src/state/seletores/index.ts
+++ src/state/seletores/index.ts
@@ -1,19 +1,32 @@
 import { selector } from "recoil";
 import { filtroDeEventos, listaDeEventosState } from "../atom";
+import { IEvento } from "../../interfaces/IEvento";
 
 export const eventosFiltradosState = selector({
   key: 'eventosFiltradosState',
   get: ({ get }) => {
-    const filtro = get(filtroDeEventos)
-    const todosOsEventos = get(listaDeEventosState)
+    const filtro = get(filtroDeEventos);
+    const todosOsEventos = get(listaDeEventosState);
     const eventos = todosOsEventos.filter(evento => {
       if (!filtro.data) {
-        return true
+        return true;
       }
       const ehOMesmoDia = filtro.data.toISOString().slice(0, 10) === evento.inicio.toISOString().slice(0, 10)
-      return ehOMesmoDia
+      return ehOMesmoDia;
     })
-    return eventos
+    return eventos;
+  }
+});
+
+export const eventosAsync = selector({
+  key: 'eventosAsync',
+  get: async () =>{
+    const respostaHTTP = await fetch('http://localhost:8080/eventos');
+    const eventosJson: IEvento[] = await respostaHTTP.json();
+    return eventosJson.map(evento => ({
+      ...evento,
+      inicio: new Date(evento.inicio),
+      fim: new Date(evento.fim)
+    }))
   }
 })
-
